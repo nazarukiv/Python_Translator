08cb20e009bb43f2194d80f1e7a8f255fce3a507
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-07-25 12:30:05 +0100

    Basic structure added and frames

---
+++ translator.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+import googletrans
+import textblob
+
+root = Tk()
+root.title("Translator")
+root.geometry("1080x400")
+
+# Icon
+image_icon = PhotoImage(file='/Users/ivan/Desktop/coding/programming/petprojects/Python_Translator/Google_Translate_Icon.png')
+root.iconphoto(False, image_icon)
+
+language = googletrans.LANGUAGES
+languageV = list(language.values())
+lang1 = language.keys()
+
+# Function to update labels based on combobox selection
+def update_label1(event):
+    label1.config(text=combo1.get())
+
+def update_label2(event):
+    label2.config(text=combo2.get())
+
+combo1 = ttk.Combobox(root, values=languageV, font="Roboto 14", state='r')
+combo1.place(x=110, y=20)
+combo1.set("English")
+combo1.bind("<<ComboboxSelected>>", update_label1)
+
+label1 = Label(root, text="English", font="segoe 30 bold", bg='white', width=18, bd=5, relief=GROOVE)
+label1.place(x=10, y=50)
+
+f = Frame(root, bg="Black", bd=5)
+f.place(x=10, y=118, width=440, height=210)
+
+text1 = Text(f, font="Roboto 20", bg='white',fg='black', relief=GROOVE, wrap=WORD)
+text1.place(x=0, y=0, width=430, height=200)
+
+scrollbar1 = Scrollbar(f)
+scrollbar1.pack(side="right", fill="y")
+
+scrollbar1.configure(command=text1.yview)
+text1.configure(yscrollcommand=scrollbar1.set)
+
+combo2 = ttk.Combobox(root, values=languageV, font="Roboto 14", state='r')
+combo2.place(x=700, y=20)
+combo2.set("Select language")
+combo2.bind("<<ComboboxSelected>>", update_label2)
+
+label2 = Label(root, text="Select language", font="segoe 30 bold", bg='white', width=18, bd=5, relief=GROOVE)
+label2.place(x=620, y=50)
+
+f1 = Frame(root, bg="Black", bd=5)
+f1.place(x=620, y=118, width=440, height=210)
+
+text2 = Text(f1, font="Roboto 20", bg='white',fg='black', relief=GROOVE, wrap=WORD)
+text2.place(x=0, y=0, width=430, height=200)
+
+scrollbar2 = Scrollbar(f1)
+scrollbar2.pack(side="right", fill="y")
+
+scrollbar2.configure(command=text2.yview)
+text2.configure(yscrollcommand=scrollbar2.set)
+
+root.configure(bg='white')
+root.mainloop()
